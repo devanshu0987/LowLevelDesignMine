73412c6f54c7b51e8b1703a1425218a9a77a6dd3
Devanshu <devanshu0987@gmail.com>
2023-09-10 07:26:08 +0530

    RandomEvictionPolicy (#7)
    Co-authored-by: Devanshu M <devansm@microsoft.com>

--- src/Cache/Program.cs
+++ src/Cache/Program.cs
@@ -15,6 +15,16 @@ namespace Cache
             cache.Add("a", "2");
             cache.Add("b", "3");
             cache.Add("b", "4");
+
+            InMemoryStorage storage2 = new();
+            RandomEvictionPolicy policy2 = new();
+            KVStore cache2 = new(storage2, policy2, 2);
+
+            cache2.Add("c", "1");
+            cache2.Add("a", "1");
+            cache2.Add("a", "2");
+            cache2.Add("b", "3");
+            cache2.Add("b", "4");
         }
     }
 }
