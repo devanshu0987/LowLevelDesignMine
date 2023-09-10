13f204cec9d87e60ee36d87de13955dd60bd09b8
Devanshu M <devansm@microsoft.com>
2023-09-10 07:25:16 +0530

    RandomEvictionPolicy

--- src/Cache/model/RandomEvictionPolicy.cs
+++ src/Cache/model/RandomEvictionPolicy.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Cache.model
 {
-    internal class RandomEvictionPolicy
+    internal class RandomEvictionPolicy : IEvictionPolicy
     {
+        /// <summary>
+        /// Dictionary is not ideal. We dont need to store their frequency of elements, nor we need to maintain it.
+        /// TODO: Reduce space.
+        /// </summary>
+        private Dictionary<string, int> keys;
+        public RandomEvictionPolicy() 
+        {
+            keys = new Dictionary<string, int>();
+        }
+
+        public string GetKeyToEvict()
+        {
+            return keys.Keys.First();
+        }
+
+        public void KeyAccessed(string key)
+        {
+            if(!keys.ContainsKey(key))
+            {
+                keys.Add(key, 1);
+            }
+        }
+
+        public void KeyEvicted(string key)
+        {
+            if (keys.ContainsKey(key))
+            {
+                keys.Remove(key);
+            }
+        }
     }
 }
